0f1e63cf03bb7a72387dede5c4406862c0c0314f
Fatima Sheremetyeva <fsheremetyeva@gmail.com>
2018-11-08 18:48:43 -0600

    Test commit

--- communityhelper/Views/footer.php
+++ communityhelper/Views/footer.php
@@ -5,7 +5,6 @@
         <p>&copy; <?php echo date('Y'); ?> Community Helper</p>
       </div>
     </footer>
-
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"  crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="Assets/js/bootstrap.min.js"></script>
